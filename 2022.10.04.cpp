694357f550731568059e8c799e988efcc5110b17
Hjg <Hjg@hjg.com>
2023-03-21 16:38:42 +0800

    提交更改

---
+++ 2022.10.04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+void simon(int n);
+using namespace std;
+
+int main(void)
+{
+	//using namespace std;
+	simon(3);
+	cout << "Pick an intege:";
+	int n;
+	cin >> n;
+	simon(n);
+	cout << "Done";
+	return 0;
+
+}
+void simon(int n)
+{
+	
+	cout << "simon says touch your toes" << n << " times." << endl;
+}
